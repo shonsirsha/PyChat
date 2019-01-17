40057e441a4c30fc2e77d4a2387531869cb91e6e
Sean Saoirse <seangeekpro@gmail.com>
2019-01-17 12:25:50 +0700

    Update init.py

--- init.py
+++ init.py
@@ -1,8 +1,8 @@
 config = {
-    "apiKey": "AIzaSyC0HKmQOfQCSFMVhiVTEf-kddB-mLz-8Cg",
-    "authDomain": "pychat-comsci.firebaseapp.com",
-    "databaseURL": "https://pychat-comsci.firebaseio.com",
-    "projectId": "pychat-comsci",
-    "storageBucket": "pychat-comsci.appspot.com",
-    "messagingSenderId": "512702370510"
+    "apiKey": "SECRET",
+    "authDomain": "SECRET",
+    "databaseURL": "SECRET",
+    "projectId": "SECRET",
+    "storageBucket": "SECRET",
+    "messagingSenderId": "SECRET"
 }
